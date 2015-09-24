dfb3c5d0f86db6fec2fca9a9cf930e39449f743c
William Butts <wbutts1023@gmail.com>
2015-09-23 23:16:15 -0400

    Added a function to find pitchers

--- moneyball_function.py
+++ moneyball_function.py
@@ -1,9 +1,20 @@
 
 
 def get_years_best(data,year):
-    ret = data[data.yearID == year].groupby('playerID').agg('mean')
+    ret = data[data.yearID == year].groupby(['playerID','G_c','G_1b','G_2b',
+                                            'G_3b','G_ss','G_of']).agg('mean')
     cleaned = ret['AB'] >100
     ret = ret[cleaned]
-    ret = (((ret.H+ret.BB+ret.HBP)/(ret.AB+ret.BB+ret.HBP+ret.SF))/ret.salary)*1000000
-    ret.order(ascending=False)
+    ret = (((ret.H+ret.BB+ret.HBP)/
+            (ret.AB+ret.BB+ret.HBP+ret.SF))/ret.salary)*1000000
+    return ret
+
+def get_top_pitchers(data,year):
+    ret = data[data.yearID == year].groupby(['playerID']).agg('mean')
+    cleaned = ret['AB'] >20
+    ret = ret[cleaned]
+    cleaned = ret.G_p > 10
+    ret = ret[cleaned]
+    ret = (((ret.H+ret.BB+ret.HBP)/
+            (ret.AB+ret.BB+ret.HBP+ret.SF))/ret.salary)*1000000
     return ret
